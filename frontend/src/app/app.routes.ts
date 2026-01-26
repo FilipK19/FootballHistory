d24ce06d07e135d613e9f58258c8f2d950d8a7c1
FilipK19 <filipkukic15@gmail.com>
2026-01-26 18:38:29 +0100

    added navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Temp } from './pages/temp/temp';
 
 export const routes: Routes = [
-    {path: '', component: Home}
+    {path: '', component: Home},
+    {path: 'temp', component: Temp}
 ];
