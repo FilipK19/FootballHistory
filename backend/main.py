2f8bf201356d6954b800dbdf97abe3d95a07494c
FilipK19 <filipkukic15@gmail.com>
2026-02-02 18:46:07 +0100

    connected backend to mock api

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
+
+MOCKAPI = "http://localhost:8001/pl24"
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+@app.get("/test")
+async def get_data():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(MOCKAPI)
+    
+    return response.json()
