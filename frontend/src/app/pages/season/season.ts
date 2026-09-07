e29f769e062f63b76e67e3e238c02d760c83b084
FilipK19 <filipkukic15@gmail.com>
2026-09-07 19:53:33 +0200

    changed season tab buttons

--- frontend/src/app/pages/season/season.ts
+++ frontend/src/app/pages/season/season.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { Player } from '../../assets/player/player';
+
 @Component({
   selector: 'app-season',
-  imports: [RouterModule],
+  imports: [RouterModule, Player],
   templateUrl: './season.html',
   styleUrl: './season.css',
 })
 export class Season {
-
+  playerColor = '#ffffff';
 }
