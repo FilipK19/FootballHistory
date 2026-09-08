636cb80e42229d3f1757b1d52c025dd119161092
FilipK19 <filipkukic15@gmail.com>
2026-09-08 18:59:38 +0200

    made buttons display corresponding matches that are saved

--- frontend/src/app/pages/season/season.ts
+++ frontend/src/app/pages/season/season.ts
@@ -1,19 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Api } from '../../services/api';
+import {KeyValuePipe} from '@angular/common';
 
 import { Player } from '../../assets/player/player';
 
 @Component({
   selector: 'app-season',
-  imports: [RouterModule, Player],
+  imports: [RouterModule, Player, KeyValuePipe],
   templateUrl: './season.html',
   styleUrl: './season.css',
 })
+
 export class Season {
   playerColor = '#00008b';
   selectedSeason = signal(1);
-  allMinfoData = signal<Record<string, any[]>>({});
+  allMinfoData = signal<Record<string, Record<string, any[]>>>({});
 
   constructor(private api: Api) {}
 
@@ -30,4 +32,18 @@ export class Season {
       this.allMinfoData.set(data);
     });
   }
+
+  // Computes the key for the selected season based on the signal
+  selectedSeasonKey = computed(() => {
+    switch (this.selectedSeason()) {
+      case 1: return 'season24';
+      case 2: return 'season23';
+      case 3: return 'season22';
+      default: return 'season24';
+    }
+  });
+
+  selectedSeasonData = computed(() =>
+    this.allMinfoData()[this.selectedSeasonKey()] ?? {}
+  );
 }
